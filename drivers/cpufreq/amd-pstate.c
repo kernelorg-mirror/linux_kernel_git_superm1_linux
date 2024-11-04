cfb9180cfa942596133b6912e72d7399cd4b73e2
Mario Limonciello <mario.limonciello@amd.com>
2024-11-04 11:54:05 -0600

    cpufreq/amd-pstate: Disable preferred cores on designs with workload classification
    On designs that have workload classification, it's preferred that
    the amd-hfi driver is used to provide hints to the scheduler of
    which cores to use instead of the amd-pstate driver.
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Perry Yuan <perry.yuan@amd.com>
    Link: https://lore.kernel.org/r/20241104175407.19546-11-mario.limonciello@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -780,6 +780,12 @@ static void amd_pstate_init_prefcore(struct amd_cpudata *cpudata)
 	/* user disabled or not detected */
 	if (!amd_pstate_prefcore)
 		return;
+	/* should use amd-hfi instead */
+	if (cpu_feature_enabled(X86_FEATURE_AMD_WORKLOAD_CLASS) &&
+	    IS_ENABLED(CONFIG_AMD_HFI)) {
+		amd_pstate_prefcore = false;
+		return;
+	}
 
 	cpudata->hw_prefcore = true;
 
