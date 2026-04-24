8b8db0b6bd5b5d809bc0ad059de4231745d8c410
Mario Limonciello <mario.limonciello@amd.com>
2026-04-24 10:47:00 -0500

    drm/amd/display: use drm backlight
    Convert AMD display driver to use the new DRM backlight infrastructure.
    This allows brightness control via DRM connector properties.
    
    Includes fixes for backlight resource lifecycle management.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -86,6 +86,7 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_uapi.h>
 #include <drm/drm_atomic_helper.h>
+#include <drm/drm_backlight.h>
 #include <drm/drm_blend.h>
 #include <drm/drm_fixed.h>
 #include <drm/drm_fourcc.h>
@@ -5424,9 +5425,32 @@ amdgpu_dm_register_backlight_device(struct amdgpu_dm_connector *aconnector)
 		dm->actual_brightness[aconnector->bl_idx] = real_brightness;
 		dm->brightness[aconnector->bl_idx] = real_brightness;
 	}
-	drm_dbg_driver(drm, "DM: Registered Backlight device: %s\n", bl_name);
+
+	/* Only set backlight name if DRM backlight was allocated successfully.
+	 * drm_backlight_alloc() may have failed earlier in init_helper, leaving
+	 * aconnector->base.backlight as NULL.
+	 */
+	if (aconnector->base.backlight) {
+		r = drm_backlight_set_name(aconnector->base.backlight, bl_name);
+		if (r)
+			goto error_cleanup_backlight;
+
+		drm_dbg_driver(drm, "DM: Registered Backlight device: %s\n", bl_name);
+	} else {
+		drm_warn(drm, "DM: Backlight device registration skipped - DRM backlight not allocated\n");
+	}
 
 	return 0;
+
+error_cleanup_backlight:
+	/* Clean up hardware backlight device if DRM backlight name setting failed */
+	backlight_device_unregister(dm->backlight_dev[aconnector->bl_idx]);
+	dm->backlight_dev[aconnector->bl_idx] = NULL;
+
+	/* Also clean up DRM backlight structure to avoid resource leak */
+	drm_backlight_free(&aconnector->base);
+
+	return r;
 }
 
 static int initialize_plane(struct amdgpu_display_manager *dm,
@@ -7880,6 +7904,10 @@ static void amdgpu_dm_connector_destroy(struct drm_connector *connector)
 	aconnector->dc_sink = NULL;
 
 	drm_dp_cec_unregister_connector(&aconnector->dm_dp_aux.aux);
+	/* DRM core will handle drm_backlight cleanup via:
+	 * drm_connector_unregister() -> drm_backlight_unregister()
+	 * drm_connector_cleanup() -> drm_backlight_free()
+	 */
 	drm_connector_unregister(connector);
 	drm_connector_cleanup(connector);
 	kfree(aconnector->dm_dp_aux.aux.name);
@@ -9129,6 +9157,12 @@ void amdgpu_dm_connector_init_helper(struct amdgpu_display_manager *dm,
 	aconnector->base.state->max_bpc = 16;
 	aconnector->base.state->max_requested_bpc = aconnector->base.state->max_bpc;
 
+	if (connector_type == DRM_MODE_CONNECTOR_eDP) {
+		int r = drm_backlight_alloc(&aconnector->base);
+
+		if (r)
+			drm_err(dm->ddev, "Failed to allocate backlight: %d\n", r);
+	}
 	if (connector_type == DRM_MODE_CONNECTOR_HDMIA) {
 		/* Content Type is currently only implemented for HDMI. */
 		drm_connector_attach_content_type_property(&aconnector->base);
